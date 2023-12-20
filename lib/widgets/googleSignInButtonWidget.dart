d5ed4dd0e83622b1047108143aebcf0c507d38e1
Rishit Reddy <80318887+Rishit-Reddy@users.noreply.github.com>
2023-12-20 14:52:45 +0100

    Update Login Screen
    Added a google icon
    changed the widget structure in login_ui.dart

---
+++ lib/widgets/googleSignInButtonWidget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButtonWidget extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const GoogleSignInButtonWidget({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: Colors.white,
+      elevation: 2.0,
+      height: 40.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/google_icon.png', // Replace with the path to your Google logo asset
+            height: 18.0, // Adjust the height as needed
+          ),
+          const SizedBox(width: 12.0),
+          const Text(
+            'Sign in with Google',
+            style: TextStyle(fontSize: 18, color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
