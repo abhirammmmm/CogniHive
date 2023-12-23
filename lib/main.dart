45ea84dd6ca54c69ed9a5719bd0d9b6deaab423f
Venkata Sai Abhiram Akula <abhiram.akula7@gmail.com>
2023-12-23 18:05:29 +0100

    to rebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:cognihive_version1/screens/Login/login_ui.dart';
+import 'package:cognihive_version1/screens/home/profile_ui.dart';
 import 'package:flutter/material.dart';
 
 void main() {
