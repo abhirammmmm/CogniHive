f407db5af4904eb59b908ed42c955cd497b90738
Rishit Reddy <80318887+Rishit-Reddy@users.noreply.github.com>
2023-12-26 18:31:44 +0100

    Save New User Profile

---
+++ lib/models/user_data.dart
@@ -0,0 +1,39 @@
+class UserData {
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String dateOfBirth;
+  String gender;
+  String college;
+
+  UserData({
+    this.firstName = '',
+    this.lastName = '',
+    this.phoneNumber = '',
+    this.dateOfBirth = '',
+    this.gender = 'Male',
+    this.college = 'BTH',
+  });
+
+  // Add a method to populate data from a map (useful when fetching data from Firestore)
+  UserData.fromMap(Map<String, dynamic> map)
+      : firstName = map['firstName'] ?? '',
+        lastName = map['lastName'] ?? '',
+        phoneNumber = map['phoneNumber'] ?? '',
+        dateOfBirth = map['dateOfBirth'] ?? '',
+        gender = map['gender'] ?? 'Male',
+        college = map['college'] ?? 'BTH';
+
+  get uid => null;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'phoneNumber': phoneNumber,
+      'dateOfBirth': dateOfBirth,
+      'gender': gender,
+      'college': college,
+    };
+  }
+}
