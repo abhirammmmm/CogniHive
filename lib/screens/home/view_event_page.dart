9ba2d206615fda62d2822229e5467fd8c6f4702c
Rishit Reddy <80318887+Rishit-Reddy@users.noreply.github.com>
2024-01-01 13:11:15 +0100

    Solve Event Page Bug

--- lib/screens/home/view_event_page.dart
+++ lib/screens/home/view_event_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import intl package
 
 class ViewEventPage extends StatelessWidget {
   final Map<String, dynamic> eventData;
@@ -12,74 +13,119 @@ class ViewEventPage extends StatelessWidget {
     final isCreatedByUser =
         FirebaseAuth.instance.currentUser?.uid == eventData['createdBy'];
 
+    var themeData = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''), // Empty text to remove the app bar text
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        iconTheme: IconThemeData(
-          color: Theme.of(context).brightness == Brightness.dark
-              ? Colors.white
-              : const Color(0xFF1E2832), // Menu icon color for light mode
-        ),// Menu icon color
+        backgroundColor: themeData.appBarTheme.backgroundColor,
+        iconTheme: themeData.appBarTheme.iconTheme,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              eventData['eventName'] ?? 'Unnamed Event',
-              style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange[800]),
-            ),
-            SizedBox(height: 20),
-            FutureBuilder<DocumentSnapshot>(
-              future: FirebaseFirestore.instance
-                  .collection('users')
-                  .doc(eventData['createdBy'])
-                  .get(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                }
-                if (snapshot.hasError) {
-                  return Text('Created by: Error loading user data',
-                      style: TextStyle(fontSize: 16, color: Colors.red));
-                }
-                if (!snapshot.hasData || snapshot.data!.data() == null) {
-                  return Text('Created by: User not found',
-                      style: TextStyle(fontSize: 16, color: Colors.grey));
-                }
-                var userData = snapshot.data!.data() as Map<String, dynamic>;
-                return Text('Created by: ${userData['displayName'] ?? 'N/A'}',
-                    style:
-                    TextStyle(fontSize: 18, fontWeight: FontWeight.w500));
-              },
-            ),
-            // Text(eventData['eventUID'] ?? "Null"),
-            SizedBox(height: 12),
-            // Event Venue and Time
-            Text('Venue: ${eventData['location'] ?? 'TBD'}',
-                style: TextStyle(fontSize: 18)),
-            SizedBox(height: 12),
-            Text('Date and Time: ${eventData['date']} at ${eventData['time']}',
-                style: TextStyle(fontSize: 18)),
-            SizedBox(height: 20),
-            // Event Description
-            Text(eventData['description'] ?? 'No description provided.',
-                style: TextStyle(fontSize: 18), textAlign: TextAlign.justify),
-            SizedBox(height: 20),
+            EventHeader(eventData: eventData),
+            CreatedBy(eventData: eventData),
+            SizedBox(height: 10),
+            EventDetails(eventData: eventData),
             if (!isCreatedByUser)
               InterestedToggleButton(eventId: eventData['eventUID']),
             if (isCreatedByUser)
               AnnouncementsSection(eventId: eventData['eventUID']),
+            Divider(),
+            SizedBox(height: 10),
+            Text('Announcements',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+            SizedBox(height: 10),
             AnnouncementsViewer(eventId: eventData['eventUID']),
           ],
         ),
       ),
+    );
+  }
+}
+
+class EventHeader extends StatelessWidget {
+  final Map<String, dynamic> eventData;
+
+  const EventHeader({Key? key, required this.eventData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: Text(
+        eventData['eventName'] ?? 'Unnamed Event',
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.orange[800],
+        ),
+      ),
+    );
+  }
+}
+
+class EventDetails extends StatelessWidget {
+  final Map<String, dynamic> eventData;
+
+  const EventDetails({Key? key, required this.eventData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Venue: ${eventData['location'] ?? 'TBD'}',
+            style: TextStyle(fontSize: 18)),
+        SizedBox(height: 12),
+        Text('Date and Time: ${eventData['date']} at ${eventData['time']}',
+            style: TextStyle(fontSize: 18)),
+        SizedBox(height: 20),
+        Text(
+          "Description",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Text(eventData['description'] ?? 'No description provided.',
+            style: TextStyle(fontSize: 18), textAlign: TextAlign.justify),
+        SizedBox(height: 20),
+      ],
+    );
+  }
+}
+
+class CreatedBy extends StatelessWidget {
+  const CreatedBy({
+    super.key,
+    required this.eventData,
+  });
+
+  final Map<String, dynamic> eventData;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('users')
+          .doc(eventData['createdBy'])
+          .get(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        }
+        if (snapshot.hasError) {
+          return Text('Created by: Error loading user data',
+              style: TextStyle(fontSize: 16, color: Colors.red));
+        }
+        if (!snapshot.hasData || snapshot.data!.data() == null) {
+          return Text('Created by: User not found',
+              style: TextStyle(fontSize: 16, color: Colors.grey));
+        }
+        var userData = snapshot.data!.data() as Map<String, dynamic>;
+        return Text('Created by: ${userData['displayName'] ?? 'N/A'}',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500));
+      },
     );
   }
 }
@@ -107,11 +153,14 @@ class _InterestedToggleButtonState extends State<InterestedToggleButton> {
         });
       },
       style: ElevatedButton.styleFrom(
-        backgroundColor: isInterested ? const Color(0xFF1E2832) : const Color(0xFF1E2832),
+        backgroundColor:
+            isInterested ? Colors.orange[500] : Colors.orange[800],
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0), // Set border radius to 2 pixels
+          borderRadius:
+              BorderRadius.circular(8.0), // Set border radius to 2 pixels
         ),
-        minimumSize: Size(double.infinity, 38), // Set width to double.infinity and height to 48
+        minimumSize: Size(double.infinity,
+            38), // Set width to double.infinity and height to 48
       ),
       child: Text(
         isInterested ? 'Not Interested' : 'Interested',
@@ -119,7 +168,6 @@ class _InterestedToggleButtonState extends State<InterestedToggleButton> {
       ),
     );
   }
-
 }
 
 class AnnouncementsSection extends StatefulWidget {
@@ -177,10 +225,6 @@ class _AnnouncementsSectionState extends State<AnnouncementsSection> {
             ),
           ),
         ),
-        SizedBox(height: 10),
-        Text('Announcements',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-        SizedBox(height: 10),
       ],
     );
   }
@@ -209,14 +253,26 @@ class AnnouncementsViewer extends StatelessWidget {
           return Text('No announcements yet.');
         }
 
-        return ListView(
-          children: snapshot.data!.docs.map((doc) {
-            var data = doc.data() as Map<String, dynamic>;
-            return ListTile(
-              title: Text(data['text']),
-              subtitle: Text(data['timestamp'].toDate().toString()),
-            );
-          }).toList(),
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.4,
+          ),
+          child: ListView(
+            shrinkWrap: true,
+            children: snapshot.data!.docs.map((doc) {
+              var data = doc.data() as Map<String, dynamic>;
+              var formattedTimestamp = '';
+              if (data['timestamp'] != null) {
+                DateTime timestamp = data['timestamp'].toDate();
+                formattedTimestamp =
+                    DateFormat('HH:mm, MMM d, yyyy').format(timestamp);
+              }
+              return ListTile(
+                title: Text(data['text']),
+                subtitle: Text(formattedTimestamp),
+              );
+            }).toList(),
+          ),
         );
       },
     );
